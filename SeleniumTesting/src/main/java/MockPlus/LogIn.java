802b4e1bac24cddc45aa5e26b1f42bc50566511c
Tanmoy <Tanmoy@DESKTOP-M2FAU9A>
2022-05-31 00:16:35 +0530

    New POM Class Added

--- SeleniumTesting/src/main/java/MockPlus/LogIn.java
+++ SeleniumTesting/src/main/java/MockPlus/LogIn.java
@@ -1,7 +1,9 @@
 package MockPlus;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LogIn {
 	
@@ -13,5 +15,28 @@ public class LogIn {
 	
 	@FindBy(xpath="//button[text()='Sign in']")
 	private WebElement login;
+
+
+
+
+public LogIn (WebDriver driver)
+{
+	PageFactory.initElements(driver, this);
+}
+
+public void csendtoemail()
+{
+	email.sendKeys("findtanmoyghosh");
+}
+
+public void sentopassword()
+{
+	password.sendKeys("Tanmoy");
+}
+
+public void clickonlogin()
+{
+	login.click();
+}
 
 }
